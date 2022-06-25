a28bc8b122b3ee6eced6661509954bd8dc9897ee
Matheus Teixeira <matheusteixeira@devnine.tech>
2022-06-25 17:19:35 -0300

    refactor: move sut creatin to a factory helper method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,9 +1,13 @@
 import { MissingParamError } from './../errors/missing-param-error'
 import { SignUpController } from './signup'
 
+const makeSut = (): SignUpController => {
+  return new SignUpController()
+}
+
 describe('SignUp Controller', () => {
   test('should return 400 if no name is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
 
     const httpRequest = {
       body: {
@@ -21,7 +25,7 @@ describe('SignUp Controller', () => {
 
 describe('SignUp Controller', () => {
   test('should return 400 if no email is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
 
     const httpRequest = {
       body: {
@@ -39,7 +43,7 @@ describe('SignUp Controller', () => {
 
 describe('SignUp Controller', () => {
   test('should return 400 if no password is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
 
     const httpRequest = {
       body: {
@@ -57,7 +61,7 @@ describe('SignUp Controller', () => {
 
 describe('SignUp Controller', () => {
   test('should return 400 if no passwordConfirmation is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
 
     const httpRequest = {
       body: {
