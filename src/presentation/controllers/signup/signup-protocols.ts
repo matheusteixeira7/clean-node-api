40f6031958e489c91d646062a9e91093a5e24728
Matheus Teixeira <matheusteixeira@devnine.tech>
2022-06-26 01:01:38 -0300

    refactor: move all signup protocols to a single file

---
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -0,0 +1,5 @@
+export * from '@/domain/models/account'
+export * from '@/domain/usecases/add-account'
+export * from '@/presentation/protocols'
+export * from '@/presentation/protocols/email-validator'
+//
