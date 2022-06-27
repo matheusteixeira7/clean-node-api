b7da428c5d5d53864e8cb48e661dbee97d484586
Matheus Teixeira <matheusteixeira@devnine.tech>
2022-06-27 14:30:23 -0300

    chore: add jest configs and scripts

--- src/presentation/controllers/signup/signup-protocols.ts
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -1,5 +1,5 @@
+/* istanbul ignore file */
 export * from '@/domain/models/account'
 export * from '@/domain/usecases/add-account'
 export * from '@/presentation/protocols'
 export * from '@/presentation/protocols/email-validator'
-//
