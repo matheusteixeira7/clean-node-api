4e849470ef7e1d89d9cd3808335b64f929f22c82
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-06-30 20:44:24 -0300

    chore: add jest ignore on protocols file

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,4 +1,5 @@
+/* istanbul ignore file */
+export * from '@/data/protocols/add-account-repository'
 export * from '@/data/protocols/encrypter'
 export * from '@/domain/models/account'
 export * from '@/domain/usecases/add-account'
-export * from '@/data/protocols/add-account-repository'
